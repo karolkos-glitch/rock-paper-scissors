b9a1007dea06b01b8046101d3ca926c11527b109
Karol <karol99kos@gmail.com>
2023-02-01 18:19:09 +0100

    Add some transition to the steps

--- app/src/components/Playground/ChoicesInformationWrapper/ChoicesInformationWrapper.tsx
+++ app/src/components/Playground/ChoicesInformationWrapper/ChoicesInformationWrapper.tsx
@@ -1,42 +1,49 @@
+import { Fragment } from "react";
 import type { GAME_CHOICES } from "@rps/settings";
 import Option from "@rps/components/Playground/Option";
 import ChoicePresentation from "./ChoicePresentation";
+import { Transition } from "@headlessui/react";
+import FadeInTransitionWrapper from "@rps/components/FadeInTransitionWrapper";
 
 interface ChoicesInformationWrapperProps {
   userChoice: GAME_CHOICES | null;
   houseChoice: GAME_CHOICES | null;
   children?: React.ReactNode;
+  shouldEnterAnimationAppear?: boolean;
 }
 
 const ChoicesInformationWrapper = ({
   children = null,
   userChoice,
   houseChoice,
+  shouldEnterAnimationAppear = false
 }: ChoicesInformationWrapperProps) => {
   const placeHolderStyles =
     "w-[150px] h-[150px] sm:w-[200px] sm:h-[200px] rounded-[50%] bg-bg-2";
   return (
-    <div className="grid grid-flow-col justify-center items-center gap-16">
-      <ChoicePresentation label="YOU PICKED">
-        {userChoice ? (
-          <Option type={userChoice} />
-        ) : (
-          <div className={placeHolderStyles} />
-        )}
-      </ChoicePresentation>
-      {children ? (
-        <div className="mx-4 text-center row-start-2 row-end-3 col-start-1 col-end-3 md:row-start-1 md:row-end-2 md:col-start-2 md:col-end-3">
-          {children}
-        </div>
-      ) : null}
-      <ChoicePresentation label="THE HOUSE PICKED">
-        {houseChoice ? (
-          <Option type={houseChoice} />
-        ) : (
-          <div className={placeHolderStyles} />
-        )}
-      </ChoicePresentation>
-    </div>
+    <FadeInTransitionWrapper appear={shouldEnterAnimationAppear}>
+      <div className="grid grid-flow-col justify-center items-center gap-16 m-w-full">
+        <ChoicePresentation label="YOU PICKED">
+          {userChoice ? (
+            <Option type={userChoice} />
+          ) : (
+            <div className={placeHolderStyles} />
+          )}
+        </ChoicePresentation>
+        {children ? (
+          <div className="mx-4 text-center row-start-2 row-end-3 col-start-1 col-end-3 md:row-start-1 md:row-end-2 md:col-start-2 md:col-end-3">
+            {children}
+          </div>
+        ) : null}
+        <ChoicePresentation label="THE HOUSE PICKED">
+            {houseChoice ? (
+              <Option type={houseChoice} />
+            ) : (
+            <div className={placeHolderStyles} />
+          )}
+        </ChoicePresentation>
+      </div>
+    </FadeInTransitionWrapper>
   );
 };
 
--- app/src/components/Playground/InGameView/InGameView.tsx
+++ app/src/components/Playground/InGameView/InGameView.tsx
@@ -1,16 +1,19 @@
+import FadeInTransitionWrapper from "@rps/components/FadeInTransitionWrapper";
 import type { GAME_CHOICES } from "@rps/settings";
 
 interface InGameViewProps {
   renderOption: (option: GAME_CHOICES, index: number) => React.ReactNode;
 }
 
-const optionsArray = ["ROCK", "PAPER", "SCISSORS"] as const;
+const optionsArray = ["PAPER", "SCISSORS", "ROCK"] as const;
 
 const InGameView = ({ renderOption }: InGameViewProps) => {
   return (
-    <div className="grid grid-rows-2 grid-flow-col justify-center gap-8">
-      {optionsArray.map((option, index) => renderOption(option, index))}
-    </div>
+    <FadeInTransitionWrapper appear>
+      <div className="grid grid-rows-2 grid-flow-col justify-center gap-8">
+        {optionsArray.map((option, index) => renderOption(option, index))}
+      </div>
+    </FadeInTransitionWrapper>
   );
 };
 
